5c8aef0af2d15025b1e8cb79a764f28ced0ad7d5
Michael Leow <mikebelsg@yahoo.com.sg>
2014-04-06 10:32:08 +0800

    created project

---
+++ UITableViewChallenge/MLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MLAppDelegate.h
+//  UITableViewChallenge
+//
+//  Created by Michael Leow on 6/4/14.
+//  Copyright (c) 2014 Michael Leow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewChallenge/MLViewController.h
@@ -0,0 +1,13 @@
+//
+//  MLViewController.h
+//  UITableViewChallenge
+//
+//  Created by Michael Leow on 6/4/14.
+//  Copyright (c) 2014 Michael Leow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLViewController : UIViewController
+
+@end
